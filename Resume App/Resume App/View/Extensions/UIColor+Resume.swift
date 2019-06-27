e98d87e28a687d45e702c0b8ffc1c4f71f6d1eb2
Zach Smoroden <zach.smoroden@eventbase.com>
2019-06-26 19:47:25 -0700

    colours

---
+++ Resume App/Resume App/View/Extensions/UIColor+Resume.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor+Resume.swift
+//  Resume App
+//
+//  Created by Zach Smoroden on 2019-06-25.
+//  Copyright © 2019 Zach Smoroden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    /// The default colour for everything profile releated
+    static var profile = UIColor(red:0.28, green:0.26, blue:0.29, alpha:1.0)
+    /// The default colour for everything education related
+    static var education = UIColor(red:0.49, green:0.62, blue:0.63, alpha:1.0)
+    /// The default colour for everything skills releated
+    static var skills = UIColor(red:0.91, green:0.85, blue:0.51, alpha:1.0)
+    /// The default colour for everything experience releated
+    static var experience = UIColor(red:0.87, green:0.61, blue:0.67, alpha:1.0)
+    /// The default colour for everything expertise related
+    static var expertise = UIColor(red:0.61, green:0.35, blue:0.50, alpha:1.0)
+}
